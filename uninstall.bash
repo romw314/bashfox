787da0605823d53365b9350c402c8f9fb82f5043
romw314 <106016361+romw314@users.noreply.github.com>
2023-11-10 12:16:33 +0100

    Update uninstall.bash

--- uninstall.bash
+++ uninstall.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "UNINSTALLING BASHFOX"
-randstr="$(base64 /dev/urandom | head -c6)"
+randstr="$(base64 /dev/urandom -w6 | grep -v '/' | head -n1)"
 echo
 echo "--- CHECK ---"
 read -p "Enter $randstr to proceed: " check
@@ -11,3 +11,4 @@ rm -vrf ~/.bashfox
 grep -v 'This loads bashfox' ~/.bashrc | perl -pe 'chomp if eof' > ~/.bashrc.temp
 rm -fv ~/.bashrc
 mv -v ~/.bashrc.temp ~/.bashrc
+echo '😕 REMEMBER: Anytime you can reinstall BashFOX ☹️'
